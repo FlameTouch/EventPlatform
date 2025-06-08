73b8d83e48056551fd251c69ac0069b36fa90226
Вячеслав Лісовський <lisovskyi10022005@gmail.com>
2025-06-08 23:29:05 +0200

    implement stripe2

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -86,7 +86,7 @@ const EventDetails = async ({ params, searchParams }: EventDetailsProps) => {
       </div>
     </section>
 
-    {/* EVENTS with the same category */}
+    {/* EVENTS WITH THE SAME CATEGORY */}
     <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
       <h2 className="h2-bold">Related Events</h2>
 
