0fb8b7ac54119403ad8680722a5a226f0b739f66
Вячеслав Лісовський <lisovskyi10022005@gmail.com>
2025-06-09 18:09:07 +0200

    implement stripe

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -3,34 +3,33 @@ import { getEventById } from '@/lib/actions/event.actions'
 import { auth } from '@clerk/nextjs/server'
 
 type UpdateEventProps = {
-  params: {
-    id: string
-  }
+  params: Promise<{ id: string }>;
 }
 
-const UpdateEvent = async ({params: {id}}: UpdateEventProps) => {
+const UpdateEvent = async ({ params }: UpdateEventProps) => {
+  const { id } = await params;  
 
-    const { sessionClaims } = await auth();
-    const userId = sessionClaims?.userId as string;
+  const { sessionClaims } = await auth();
+  const userId = sessionClaims?.userId as string;
 
-    const event = await getEventById(id)
+  const event = await getEventById(id);
 
-    return (
+  return (
     <>
-        <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
-            <h3 className='wrapper h3-bold text-center sm:text-left'>Update Event</h3>
-        </section>
+      <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+        <h3 className='wrapper h3-bold text-center sm:text-left'>Update Event</h3>
+      </section>
 
-        <div className='wrapper my-8'>
-            <EventForm 
-              type="Update" 
-              event={event} 
-              eventId={event._id} 
-              userId={userId} 
-            />
-        </div>
+      <div className='wrapper my-8'>
+        <EventForm
+          type="Update"
+          event={event}
+          eventId={event._id}
+          userId={userId}
+        />
+      </div>
     </>
   )
 }
 
-export default UpdateEvent
+export default UpdateEvent;
